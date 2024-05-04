83ca0fd45d357e225f8a5c7d468881c82eb9575d
YathavakrishnanM <yathavakrishnanm.sf4659@gmail.com>
2024-05-04 09:03:34 +0530

    Web-API installation ans Use

---
+++ MedicalStoreApplication/Data/Contact.cs
@@ -0,0 +1,12 @@
+namespace MedicalStoreApplication;
+
+public class Contact
+{
+    public string? ID{ get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email{ get; set; }
+
+    public string? Phone { get; set; }
+}
